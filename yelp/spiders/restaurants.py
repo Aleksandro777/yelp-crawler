d0e37172c24747507cb5e0a4d597af608ff77602
Oleksandr Murynskyi <a.murinskiy@gmail.com>
2022-12-16 11:36:31 +0200

    Add crawler pagination

--- yelp/spiders/restaurants.py
+++ yelp/spiders/restaurants.py
@@ -17,10 +17,29 @@ class RestaurantsSpider(scrapy.Spider):
         #     f.write(response.body)
         # self.log(f"Saved file {filename}")
         for restaurant in response.css(".arrange__09f24__LDfbs "):
-            if restaurant.css(".css-1m051bw::attr(name)").get() is not None:
+            name = restaurant.css(".css-1m051bw::attr(name)").get()
+            if name is not None:
                 yield {
-                    "name": restaurant.css(".css-1m051bw::attr(name)").get(),
-                    "rating": float(restaurant.css(".five-stars--regular__09f24__DgBNj::attr(aria-label)").get().split(" ")[0]),
-                    "reviews": int(restaurant.css(".border-color--default__09f24__NPAKY > .display--inline-block__09f24__fEDiJ > .css-chan6m::text").get()),
-                    "buisness_page": urljoin(self.start_urls[0], restaurant.css(".child__09f24__Z2_cG > .css-w8rns > a::attr(href)").get()),
+                    "name": name,
+                    "rating": float(
+                        restaurant.css(
+                            ".five-stars--regular__09f24__DgBNj::attr(aria-label)"
+                        )
+                        .get()
+                        .split(" ")[0]
+                    ),
+                    "reviews": int(
+                        restaurant.css(
+                            ".border-color--default__09f24__NPAKY > .display--inline-block__09f24__fEDiJ > .css-chan6m::text"
+                        ).get()
+                    ),
+                    "buisness_page": urljoin(
+                        self.start_urls[0],
+                        restaurant.css(
+                            ".child__09f24__Z2_cG > .css-w8rns > a::attr(href)"
+                        ).get(),
+                    ),
                 }
+            next_page = response.css(".pagination-links__09f24__bmFj8 > div")[-1].css("a::attr(href)").get()
+            if next_page is not None:
+                yield scrapy.Request(next_page, callback=self.parse)
