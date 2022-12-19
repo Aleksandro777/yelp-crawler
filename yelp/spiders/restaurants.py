cd5950b46832034f7b3da65695e880b332bb7be5
Oleksandr Murynskyi <a.murinskiy@gmail.com>
2022-12-19 10:55:07 +0200

    Add parser on detail page

--- yelp/spiders/restaurants.py
+++ yelp/spiders/restaurants.py
@@ -6,40 +6,27 @@ from scrapy.http import Response
 
 class RestaurantsSpider(scrapy.Spider):
     name = "restaurants"
-    allowed_domains = ["www.yelp.com"]
+
     start_urls = [
         "https://www.yelp.com/search?cflt=restaurants&find_loc=San+Francisco%2C+CA"
     ]
 
     def parse(self, response: Response, **kwargs):
-        # filename = f"restaurants.html"
-        # with open(filename, "wb") as f:
-        #     f.write(response.body)
-        # self.log(f"Saved file {filename}")
-        for restaurant in response.css(".arrange__09f24__LDfbs "):
-            name = restaurant.css(".css-1m051bw::attr(name)").get()
-            if name is not None:
-                yield {
-                    "name": name,
-                    "rating": float(
-                        restaurant.css(
-                            ".five-stars--regular__09f24__DgBNj::attr(aria-label)"
-                        )
-                        .get()
-                        .split(" ")[0]
-                    ),
-                    "reviews": int(
-                        restaurant.css(
-                            ".border-color--default__09f24__NPAKY > .display--inline-block__09f24__fEDiJ > .css-chan6m::text"
-                        ).get()
-                    ),
-                    "buisness_page": urljoin(
-                        self.start_urls[0],
-                        restaurant.css(
-                            ".child__09f24__Z2_cG > .css-w8rns > a::attr(href)"
-                        ).get(),
-                    ),
-                }
+
+        for link in response.css(".css-1agk4wl a::attr(href)"):
+            yield response.follow(link, callback=self.parse_restaurant)
+
             next_page = response.css(".pagination-links__09f24__bmFj8 > div")[-1].css("a::attr(href)").get()
             if next_page is not None:
                 yield scrapy.Request(next_page, callback=self.parse)
+
+    def parse_restaurant(self, response: Response, **kwargs):
+        yield {
+            "name": response.css("h1.css-1se8maq::text").get(),
+            "raiting": float(response.css(".five-stars__09f24__mBKym::attr(aria-label)").get().split(" ")[0]),
+            "reviews": int(response.css(".arrange-unit__09f24__rqHTg > .css-1fdy0l5 > .css-1m051bw::text").get().split(" ")[0]),
+            "buisness_page": response.request.url,
+            "Business_website": response.css("div.css-1vhakgw > div.arrange__09f24__LDfbs > div.arrange-unit__09f24__rqHTg > .css-1p9ibgf > a.css-1um3nx::text").get()
+        }
+
+
